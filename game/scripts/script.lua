f4df89ebb6fac024049a4affeeac5582dd6cfbd9
AToska21 <58345270+AToska21@users.noreply.github.com>
2021-03-20 09:06:25 +0100

    update glitchtext
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- game/scripts/script.lua
+++ game/scripts/script.lua
@@ -223,9 +223,7 @@ end
 function glitchtext(range)
 	local aaa
 	local gtextstring = ''
-	local chars = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-				   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-				   '0','1','2','3','4','5','6','7','8','9',space(math.random(0,9))}
+	local chars = {'a',space(math.random(0,9))}
 	
 	for i = 1, range do
 		aaa = math.random(1, #chars)
